91479a08ecf7bb256d65d5fcc8e87d729bc00a3b
IamDreamcatcher <OlegVolkovskiy2002@yandex.ru>
2023-04-14 17:05:05 +0300

    first commit

---
+++ 1/SocketConnection/SocketConnection/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace SocketConnection.Entities
+{
+    internal class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public Message(int id, string text)
+        {
+            this.Id = id;
+            this.Text = text;
+        }
+
+        public Message()
+        {
+        }
+    }
+}
---
+++ 1/SocketConnection/SocketConnection/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace SocketConnection.Entities
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public int RecipientPort { get; set; }
+        public string RecipientIp { get; set; }
+        public User(string userName, string ip, int port, int recipientPort, string recipientIp)
+        {
+            this.UserName = userName;
+            this.Ip = ip;
+            this.Port = port;
+            this.RecipientPort = recipientPort;
+            this.RecipientIp = recipientIp;
+        }
+        
+        public User()
+        {
+
+        }
+    }
+}
